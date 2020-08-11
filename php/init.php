0b56697c04096a3ca9f18f92aa2a86752faaceb2
Mark Jay <markjaymercado@ph.fujitsu.com>
2020-08-11 14:45:01 +0800

    db configuration

--- php/init.php
+++ php/init.php
@@ -1,5 +1,10 @@
 <?php
 
-require ''
+require_once './functions.php';
+
+spl_autoload_register(function($classes)
+{
+    require_once $_SERVER['DOCUMENT_ROOT'].'/st-ot-qa-system/classes/'.$classes.'.php';
+})
 
 ?>
